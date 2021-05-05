42983195252c28959f3835ff318f0640992f86b8
Debadrita Ghosh <debadritaghosh0@gmail.com>
2021-05-06 04:24:11 +0530

    Update UserRepository.java

--- UserRepository.java
+++ UserRepository.java
@@ -5,12 +5,13 @@ package com.javahelps.restservice.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.javahelps.restservice.entity.User;
 
 
 @RestResource(exported = false)
-public interface UserRepository extends JpaRepository<User, String> {
+public interface UserRepository extends JpaRepository<User, String> { //declaration of user repository enables us CRUD operations)
 
 }
