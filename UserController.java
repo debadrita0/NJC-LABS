257c7d28c67f630e91d816924ad319b56c5e4830
Debadrita Ghosh <debadritaghosh0@gmail.com>
2021-05-06 04:23:23 +0530

    Update UserController.java

--- UserController.java
+++ UserController.java
@@ -49,3 +49,19 @@ else {
 throw new BadHttpRequest();}}
 
 }
+public User create(@RequestBody User user) {
+return repository.save(user);}
+
+@DeleteMapping(path = "/{username}")
+public void delete(@PathVariable("username") String username) {
+repository.deleteById(username);}
+
+@PutMapping(path = "/{username}")
+public User update(@PathVariable("username") String username, @RequestBody User user) throws BadHttpRequest {
+if (repository.existsById(username)) {
+	user.setUsername(username);
+	return repository.save(user);}
+else {
+throw new BadHttpRequest();}}
+
+}
