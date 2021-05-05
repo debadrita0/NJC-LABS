d0587157328cdc8fe43cfb81c04d9b370d22de2f
Debadrita Ghosh <debadritaghosh0@gmail.com>
2021-05-06 04:19:47 +0530

    Update Application.java

--- Application.java
+++ Application.java
@@ -1,9 +1,11 @@
 package com.javahelps.restservice;
 
 import org.springframework.boot.SpringApplication;
+
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication //meta annotation
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
